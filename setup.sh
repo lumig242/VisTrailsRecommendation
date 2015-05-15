087787d4f9371100726293790d1ce40a20d19deb
Chris <you@example.com>
2015-05-15 15:44:04 -0700

    Sleeping because mongo sucks

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #! /usr/bin/env bash
 sudo service mongodb start
+#Mongo takes a long time to start up
+echo 'Sleeping for 3 minutes because mongo takes a long time'
+sleep 60
+echo '1 minute has passed'
+sleep 60
+echo '2 minutes have passed'
+sleep 60
 mongorestore --host localhost --db PW_2012_09_02_09_03_35 --directoryperdb /home/developer/PW_2012_09_02_09_03_35 --drop 
